089654f074200cabd396b24d085369cf654d77b7
RMorgan8 <rohanmorgan7@gmail.com>
2020-01-16 14:00:31 +0000

    Added scripts

---
+++ Assets/Scripts/Door_trigger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_trigger : MonoBehaviour
+{
+    public GameObject door;
+
+    public Transform startPos;
+    public Transform endPos;
+    public float speed = 1f;
+
+    float time = .5f;
+
+    bool isOpened = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!isOpened)
+        {
+            StartCoroutine(OpenDoor());
+        }
+    }
+
+    IEnumerator OpenDoor()
+    {
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            Debug.Log(i);
+            door.transform.position = Vector3.Lerp(door.transform.position, endPos.position, i);
+            isOpened = true;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (isOpened)
+        {
+            StartCoroutine(CloseDoor());
+        }
+    }
+
+    IEnumerator CloseDoor()
+    {
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            door.transform.position = Vector3.Lerp(door.transform.position, startPos.position, i);
+            isOpened = false;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    public Camera cam;
+    public float interactDist;
+
+    public Transform holdPos;
+    public float attactspeed;
+
+    public float minThrowForce;
+    public float maxThrowForce;
+    private float throwForce;
+
+    private GameObject objectIHave;
+    private Rigidbody objectRB;
+
+    private Vector3 rotateVector = Vector3.one;
+
+    private bool hasObject = false;
+
+
+
+    private void Start()
+    {
+        throwForce = minThrowForce;
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !hasObject)
+        {
+            doRay();
+        }
+
+        if (Input.GetMouseButtonDown(1) && hasObject)
+        {
+            throwForce += 10f;
+            Debug.Log(throwForce);
+        }
+
+        if (Input.GetMouseButtonUp(1) && hasObject)
+        {
+            shootObject();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && hasObject)
+        {
+            dropObject();
+        }
+
+        if (hasObject)
+        {
+            if (checkDist() > -1f)
+            {
+                moveObjectToPos();
+            }
+        }
+
+
+
+    }
+
+    //---------Fuction
+
+    public float checkDist()
+    {
+        float dist = Vector3.Distance(objectIHave.transform.position, holdPos.transform.position);
+        return dist;
+    }
+
+    private void moveObjectToPos()
+    {
+        objectIHave.transform.position = Vector3.Lerp(objectIHave.transform.position, holdPos.position, attactspeed * Time.deltaTime);
+    }
+
+    private void dropObject()
+    {
+        objectRB.constraints = RigidbodyConstraints.None;
+        objectIHave.transform.parent = null;
+        objectIHave = null;
+        hasObject = false;
+    }
+
+    private void shootObject()
+    {
+        throwForce = Mathf.Clamp(throwForce, minThrowForce, maxThrowForce);
+        objectRB.AddForce(cam.transform.forward * throwForce, ForceMode.Impulse);
+        throwForce = minThrowForce;
+        dropObject();
+    }
+
+    private void doRay()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactDist))
+        {
+            if (hit.collider.CompareTag("Block"))
+            {
+                objectIHave = hit.collider.gameObject;
+                objectIHave.transform.SetParent(holdPos);
+
+                objectRB = objectIHave.GetComponent<Rigidbody>();
+                objectRB.constraints = RigidbodyConstraints.FreezeAll;
+
+                hasObject = true;
+            }
+        }
+    }
+}
