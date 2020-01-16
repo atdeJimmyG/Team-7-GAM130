9e7ccc9e990ef7d5f64a4728c375f900dbab42e8
Shehu12 <jesusvaquerozurita@gmail.com>
2020-01-16 13:46:26 +0000

    Added Unity Project with Fire Level Blockout

---
+++ Assets/RemovePipes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemovePipes : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
