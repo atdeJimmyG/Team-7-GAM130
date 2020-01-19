b3ffd5ca7cba231f7ae38571fa653951df67d8cd
atdeJimmyG <jamesgilljames9@gmail.com>
2020-01-19 21:21:12 +0000

    Added basic Scale spell functionality, NOT FULLY FUNCTIONAL

---
+++ Assets/Scripts/Scale/ScaleScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleScript : MonoBehaviour
+{
+    public GameObject Cube;
+
+    public float scaleValue = 0.00f;
+    int counter = 0;
+
+
+    void Update()
+    {
+        scaleValue += Input.GetAxis("Mouse ScrollWheel");
+
+        if (Input.GetMouseButton(0) && counter <=1) { 
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                Debug.Log(hit.transform.name);
+                Cube.transform.localScale += new Vector3(scaleValue, scaleValue, scaleValue);
+                //counter += 1;
+            }
+
+            if (hit.transform.name == "Cube"){
+                
+            }
+        }
+        
+    }
+}
