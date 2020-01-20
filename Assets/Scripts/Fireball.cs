1cef5350ebade44f8ca44418c2e6105a59eef863
EP231933 <EP231933@falmouth.ac.uk>
2020-01-20 13:29:23 +0000

    added a useless fireball :/

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField]
+    private Transform position;
+
+    [SerializeField]
+    private GameObject fireball;
+
+    [SerializeField]
+    private float speed = 1f;
+
+    [SerializeField]
+    private float coolDownTime = 0.2f;
+
+    private float nextFireTime = 0;
+
+    void Update()
+    {
+        if (Time.time > nextFireTime)
+        {
+            if (Input.GetMouseButtonDown(0))
+
+            {
+                FireFire();
+                nextFireTime = Time.time + coolDownTime;
+            }
+        }
+    }
+
+    private void FireFire()
+    {
+        GameObject firedBall = Instantiate(fireball, position.position, position.rotation);
+        firedBall.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
