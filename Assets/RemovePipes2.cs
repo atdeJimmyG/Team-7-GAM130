d0608bf5530f6e4b669b9ce87d5a790da670483d
Shehu12 <jesusvaquerozurita@gmail.com>
2020-01-20 10:53:31 +0000

    New fire level and script
    Added second room for Fire level prototyping. Also added a second Remove Game Object script to test the rooms until the fire mechanic works.

---
+++ Assets/RemovePipes2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemovePipes2 : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
