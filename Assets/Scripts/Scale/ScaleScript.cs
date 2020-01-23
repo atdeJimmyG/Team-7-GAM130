8e7c1286be0ce0e9b1bf9731ec27bc4fb4c647da
atdeJimmyG <jamesgilljames9@gmail.com>
2020-01-23 01:54:26 +0000

    Updated scale spell, added scale caps to make it not past certain size

--- Assets/Scripts/Scale/ScaleScript.cs
+++ Assets/Scripts/Scale/ScaleScript.cs
@@ -2,28 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScaleScript : MonoBehaviour
-{
+public class ScaleScript : MonoBehaviour {
     public GameObject Cube;
 
     public float scaleValue = 0.00f;
-    int counter = 0;
-    private float maxSize;
-    //private float cubeScale = Cube.transform.localScale;
 
-    void Update()
-    {
-        //maxSize = Mathf.Clamp(cubeScale, -0.25f, 0.25f);
+
+    void Update() {
         if (Input.GetAxis("Mouse ScrollWheel") > 0f) { // forward
             scaleValue = 0.01f;
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f) { // forward
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f) { // backwards
             scaleValue = -0.01f;
         }
         scaleValue += Input.GetAxis("Mouse ScrollWheel");
 
-        if (Input.GetMouseButton(0) && (Input.GetAxis("Mouse ScrollWheel")) != 0f) { 
+        if (Input.GetMouseButton(0) && (Input.GetAxis("Mouse ScrollWheel")) != 0f) {
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -31,18 +26,7 @@ public class ScaleScript : MonoBehaviour
             if (Physics.Raycast(ray, out hit)) {
                 Debug.Log(hit.transform.name);
                 Cube.transform.localScale += new Vector3(scaleValue, scaleValue, scaleValue);
-                
-
-                if(Cube.transform.localScale == new Vector3(0.25f,0.25f,0.25f)) {
-                ////////////WIP////////////
-                }
-                
-            }
-
-            if (hit.transform.name == "Cube"){
-                
             }
         }
-        
     }
 }
---
+++ Assets/Scripts/Scale/cubeScaleMin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScaleMin : MonoBehaviour
+{
+    private Vector3 minLocalScale;
+    float minlocalScaleMagnitude;
+
+    void Start() {
+
+        minLocalScale = new Vector3(0.5f, 0.5f, 0.5f);
+        minlocalScaleMagnitude = minLocalScale.magnitude;
+    }
+
+
+    void FixedUpdate() {
+
+        float actualLocalScaleMagnitude = transform.localScale.magnitude;
+         if (Input.GetMouseButton(0) && (Input.GetAxis("Mouse ScrollWheel")) != 0f && (actualLocalScaleMagnitude < minlocalScaleMagnitude)) {
+            transform.localScale += new Vector3(0.5F, 0.5F, 0.5f);
+        }
+    }
+}
