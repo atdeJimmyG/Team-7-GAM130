87b8e323d4ed417c9c9467dba99b1c63971c4b41
Jammes <jamesgilljames9@gmail.com>
2020-01-31 16:35:12 +0000

    Removed unnecessary code from min and max cubesSale scripts

--- Assets/Scripts/Scale/cubeScaleMax.cs
+++ Assets/Scripts/Scale/cubeScaleMax.cs
@@ -4,24 +4,12 @@ using UnityEngine;
 
 public class cubeScaleMax : MonoBehaviour
 {
-    private Vector3 maxLocalScale;
-    float maxlocalScaleMagnitude;
-
-    void Start() {
-
-        maxLocalScale = new Vector3(2f, 2f, 2f);
-        maxlocalScaleMagnitude = maxLocalScale.magnitude;
-    }
-
-
     void FixedUpdate() {
 
         float actualLocalScaleMagnitude = transform.localScale.magnitude;
         if (transform.localScale.x > 2) {
             transform.localScale = new Vector3(2f, 2f, 2f);
         }
-        /*if (Input.GetMouseButton(0) && (Input.GetAxis("Mouse ScrollWheel")) != 0f && (actualLocalScaleMagnitude > maxlocalScaleMagnitude)) {
-           transform.localScale = new Vector3(2, 2, 2);
-       }*/
+
     }
 }
--- Assets/Scripts/Scale/cubeScaleMin.cs
+++ Assets/Scripts/Scale/cubeScaleMin.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 
 public class cubeScaleMin : MonoBehaviour
 {
-    private Vector3 minLocalScale;
-    float minlocalScaleMagnitude;
-
-    void Start() {
-
-        minLocalScale = new Vector3(0.5f, 0.5f, 0.5f);
-        //minlocalScaleMagnitude = minLocalScale.magnitude;
-    }
-
-
     void FixedUpdate() {
         float actualLocalScaleMagnitude = transform.localScale.magnitude;
         if(transform.localScale.x < .5f) {
             transform.localScale = new Vector3(.5f, .5f, .5f);
         }
-        //if (Input.GetMouseButton(0) && (Input.GetAxis("Mouse ScrollWheel")) != 0f && //(actualLocalScaleMagnitude < minlocalScaleMagnitude)) {
-        //transform.localScale = new Vector3(0.5F, 0.5F, 0.5f);
+        
     }
 }
