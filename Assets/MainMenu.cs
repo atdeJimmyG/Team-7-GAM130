a06715bcc04ed86dbb3388326e19c5ee59951814
RMorgan8 <rohanmorgan7@gmail.com>
2020-02-05 11:55:22 +0000

    Added a main menu. Added the ground work for a level select Also added the ground work for a options menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public SceneFader scenefader;
+    public string NextLevel;
+
+    // When called tells thge scene fader to open the next level.
+    public void PlayGame()
+    {
+        scenefader.fadeTo(NextLevel);
+    }
+
+    // When called quits the application
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("ExitGame!");
+    }
+
+    public void OpenLevelSelect()
+    {
+        scenefader.fadeTo("");
+    }
+}
---
+++ Assets/Scripts/SceneFader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour
+{
+    public Image img;
+    public AnimationCurve curve;
+
+    void Start()
+    {
+        StartCoroutine(fadeIn());
+    }
+
+    IEnumerator fadeIn ()
+    {
+        float t = 1f;
+
+        while (t > 0f)
+        {
+            t -= Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0;
+        }
+    }
+
+    public void fadeTo(string scene)
+    {
+        StartCoroutine(fadeOut(scene));
+    }
+
+    IEnumerator fadeOut(string scene)
+    {
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0;
+        }
+
+        SceneManager.LoadScene(scene);
+    }
+}
