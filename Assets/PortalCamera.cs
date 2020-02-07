bf60425b9628996300df763ff003620e1eb3b3d3
atdeJimmyG <jamesilljames9@gmail.com>
2020-02-07 11:24:19 +0000

    Added Portal test, basic functionality working, tweaking needed for values and bug where teleports back if goes beyond portal location

---
+++ Assets/PortalCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform Portal;
+    public Transform otherPortal;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = Portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(Portal.rotation, otherPortal.rotation);
+
+        Quaternion portalrotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalrotationalDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+}
---
+++ Assets/portalTeleporter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalTeleporter : MonoBehaviour
+{
+    public CharacterController Player;
+    public Transform Reciever;
+
+    bool PlayerIsOverlapping = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerIsOverlapping) {
+            Vector3 portalToPlayer = Player.transform.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if(dotProduct < 0f) {
+                float rotationDiff = Quaternion.Angle(transform.rotation, Reciever.rotation);
+                rotationDiff += 180;
+                Player.transform.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                Player.enabled = false;
+                Player.transform.position = Reciever.position + positionOffset;
+                Player.enabled = true;
+
+                PlayerIsOverlapping = false;
+
+            }
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player"){
+            PlayerIsOverlapping = true;
+        }
+    }
+
+    void onTriggerExit(Collider other) {
+        if (other.tag == "Player") {
+            PlayerIsOverlapping = false;
+        }
+    }
+}
