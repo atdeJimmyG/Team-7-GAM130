2e4fae6af826d80579a35f27bd0ea7e083bfae40
atdeJimmyG <jamesgilljames9@gmail.com>
2020-04-29 12:30:06 +0100

    Added Glass Break in telekinesis scene

---
+++ Assets/Fragile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fragile : MonoBehaviour {
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Block") {
+            Destroy(gameObject);
+        }
+    }
+}
